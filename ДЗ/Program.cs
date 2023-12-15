358d263e293ea844c4eb7af6993277fc8d206afa
mikhail <trushakin@list.ru>
2023-12-15 20:50:59 +0300

    решил вторую задачу

--- ДЗ/Program.cs
+++ ДЗ/Program.cs
@@ -2,23 +2,37 @@
 //Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90]
 
 
-// int[] array = new int [10];
-//  array[i] = new Random().Next(1,101); 
-//  System.Console.Write(array[i]+"");
+// int[] array = new int[10];
 
-//  for (int i=0; i<array.Length; i++)
+// for(int i=0; i<array.Length; i++)
+// {
+// array[i] = new Random().Next(1,101);
+// System.Console.Write(array[i]+ " ");
+// }
+// int num=0;
+// for(int i=0; i<array.Length; i++)
+// {
+// if(array[i]>19 && array[i]<91 )
+// num++;
+// }
+// System.Console.WriteLine("=>"+" "+num);
 
-int[] array = new int[10];
 
-for(int i=0; i<array.Length; i++)
-{
-array[i] = new Random().Next(1,101);
-System.Console.Write(array[i]+ " ");
-}
-int num=0;
-for(int i=0; i<array.Length; i++)
-{
-if(array[i]>19 && array[i]<91 )
-num++;
-}
-System.Console.WriteLine("=>"+"\t"+num);
+
+//Задача 2: Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+
+// int[] array = new int[10];
+
+// for(int i=0; i<array.Length; i++)
+// {
+// array[i] = new Random().Next(1,101);
+// System.Console.Write(array[i]+ " ");
+// }
+// int num=0;
+// for(int i=0; i<array.Length; i++)
+// {
+// if(array[i]%2==0) num++;
+// }
+// System.Console.WriteLine("=>"+" "+num);
+
+
